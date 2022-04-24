edeb5afc8405702ae9ad54bb63596c5889351093
julytatiana <yulytatiana@live.com>
2022-04-24 12:02:34 -0500

    Parte 1 del ejercicio completado en rama July

--- main.js
+++ main.js
@@ -38,6 +38,6 @@
 window.addEventListener("load", main);
 function main() {
     var board = new Board(800, 400);
-    var canvas = document.getElementById("canvas", board);
-    var board_view = new BoardView();
+    var canvas = document.getElementById('canvas');
+    var board_view = new BoardView(canvas, board);
 }
